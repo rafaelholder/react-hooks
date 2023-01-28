1e126b4fe0d21c078e665bbf72f5c9728b49b7e5
Rafael <rafaelholder0106@gmail.com>
2023-01-28 17:29:56 -0300

    useRef, useCallbak and react.memo

--- src/App.js
+++ src/App.js
@@ -1,66 +1,83 @@
+/* eslint-disable no-unused-vars */
 import logo from './logo.svg';
+import P from 'prop-types';
 import './App.css';
-import { useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+  useRef,
+} from 'react';
 
-function App() {
-  // const { reverse } = this.state;
-  const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+// // React.memo has the same propose of useCallback
+// const Button = ({ handleIncrementCounter }) => {
+//   return (
+//     <button onClick={() => handleIncrementCounter(1000)}> Increment +</button>
+//   );
+// };
+// Button.propTypes = {
+//   handleIncrementCounter: P.func,
+// };
 
-  const eventFn = () => {
-    console.log('h1 clicado');
-  };
+// function App() {
+//   const [reverse, setReverse] = useState(false);
+//   const [counter, setCounter] = useState(0);
 
-  // HOOKS RULES https://reactjs.org/docs/hooks-rules.html //
-  //////////////////////////////////////////////////////////
+//   const reverseClass = reverse ? 'reverse' : '';
 
-  // // ComponentDidUpdate - execute every time a component update
-  // useEffect(() => {
-  //   console.log('component Did Update');
-  // });
+//   // HOOKS RULES https://reactjs.org/docs/hooks-rules.html //
+//   //////////////////////////////////////////////////////////
 
-  // // ComponentDidMount - execute every time a component is mounted
-  // useEffect(() => {
-  //   console.log('component Did Mount');
-  // }, []);
+//   // // ComponentDidUpdate - execute every time a component update
+//   // useEffect(() => {
+//   //   console.log('component Did Update');
+//   // });
 
-  // ComponentDidMount - execute every time a dependencies is changed
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
+//   // // ComponentDidMount - execute every time a component is mounted
+//   // useEffect(() => {
+//   //   console.log('component Did Mount');
+//   // }, []);
 
-    //ComponentWillUnmount - Clear the dirty saved in memory
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, [counter]);
+//   // ComponentDidMount - execute every time a dependencies is changed
+//   // useEffect(() => {
+//   //   document.querySelector('h1')?.addEventListener('click', eventFn);
 
-  const handleClick = () => {
-    // this.setState({ reverse: !reverse });
-    //Normal hook
-    setReverse(!reverse);
-  };
-  const handleIncrement = () => {
-    //CallBack function
-    setCounter((prevCounter) => prevCounter + 1);
-  };
+//   //   //ComponentWillUnmount - Clear the dirty saved in memory
+//   //   return () => {
+//   //     document.querySelector('h1')?.removeEventListener('click', eventFn);
+//   //   };
+//   // }, [counter]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>Counter: {counter}</h1>
+//   const handleClick = () => {
+//     // this.setState({ reverse: !reverse });
+//     //Normal hook
+//     setReverse(!reverse);
+//   };
+//   const handleIncrementCounter = useCallback((num) => {
+//     setCounter((prevCounter) => {
+//       prevCounter + num;
+//     });
+//   }, []);
 
-        <button type="button" onClick={handleClick}>
-          Reverse {reverseClass}
-        </button>
-        <button type="button" onClick={handleIncrement}>
-          increment {counter}
-        </button>
-      </header>
-    </div>
-  );
-}
+//   const btn = useMemo(() => {
+//     return <Button handleIncrementCounter={handleIncrementCounter} />;
+//   }, [handleIncrementCounter]);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+//         <button type="button" onClick={handleClick}>
+//           Reverse {reverseClass}
+//         </button>
+
+//         <h1>Counter: {counter}</h1>
+//         {btn}
+//       </header>
+//     </div>
+//   );
+// }
 
 // class App extends Component {
 //   state = {
@@ -91,5 +108,81 @@ function App() {
 //     );
 //   }
 // }
+
+const Post = ({ post, handleClick }) => {
+  console.log('Son component render');
+  return (
+    <div key={post.id} className="post">
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+  handleClick: P.func,
+};
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+
+  const inputRef = useRef(null); //UseRef dont re-render the page/component
+
+  const counter = useRef(0); //Count the many times the page render
+
+  console.log(' render!');
+
+  // Component did mount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
+  }, []);
+
+  useEffect(() => {
+    inputRef.current.focus();
+    // Current is a useRef value.
+    console.log(inputRef.current);
+  }, [value]);
+
+  useEffect(() => {
+    counter.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
+
+  return (
+    <div className="App">
+      <h6>Render: {counter.current}x</h6>
+      <p>
+        <input
+          ref={inputRef}
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>There are no posts.</p>}
+    </div>
+  );
+}
 
 export default App;
